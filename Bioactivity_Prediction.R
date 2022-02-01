f101a3dee15e6453d7a65eefd4caae3e2ca87f9e
Amirreza-Mousavi <sam00mb@gmail.com>
2022-02-01 18:51:10 +0330

    6:51

--- Bioactivity_Prediction.R
+++ Bioactivity_Prediction.R
@@ -1,40 +1,72 @@
+###Initialization
 library(Rcpi)
-rsq = function(x,y){cor(x,y)*cor(x,y)}
-Jaccardindex=function(A,B){
-  int=table(A&B)[2]
-  return(as.numeric(int/(length(A)+length(B)-int)))
-  
-}
-x=readMolFromSmi("Galaxy18-[Convert_to_smi_from_data_16].smi")
+library(rpart)
+library(dplyr)
+library(e1071)
+library(stringr)
 
+set.seed(93)
+
+rsq = function(x,y){
+  cor(x,y)*cor(x,y)
+  }
+
+###Import the chemicals
+x=readMolFromSmi("Smile_Files.smi")
+
+###Compute MACCS166 chemical fingerprints for the molecules
 y=extractDrugMACCSComplete(x)
 
-###Load Bioactivity
+###Load Bioactivity data taken from ChEMBL
 bio=read.table("Bio2.csv",header = T)
+
+###Process NAs. Delete them from both data, bio(Bioactivity data) and y(MACCS166 matrix)
 ind=which(is.na(bio))
 bio=na.omit(bio)
-library(dplyr)
 y=as.data.frame(y)
 y=y[-ind,]
 y$bioactivity=(bio[,1])
+
+###Take a look at the data used for the decision tree algorithm
+head(y)
 y
-###############
-library(rpart)
 dim(y)
-set.seed(11)
-sind=sample(1:124,70)
+
+###Prepare test and train datasets
+N=70 # N = amount of data used for training algorithm out of all data = dim(y)[1] = 124
+sind=sample(1:(dim(y)[1]),70)
 train=y[sind,]
 test=y[-sind,]
+
+###Take a look at train and test datasets
+head(train)
+head(test)
 dim(train)
 dim(test)
-#################
-library(e1071)
+
+###Implement decision tree
 tree=rpart((bioactivity)~.,train)
 prd=predict(tree,test[,-167])
-
 real=test[,167]
-prd=(prd)
-real
 
-table(predicted=round(prd),reality=real)
+###Print the confusion matrix
+res=table(predicted=round(prd),reality=real)
+print(res)
+
+###Accuracy = ?
+cat(str_c("Accuracy is ",sum(diag(res))/sum(res),"\n"))
+
+
+
+###Implement SVM
+mod=svm(as.factor(bioactivity)~.,train,scale = F)
+prd=predict(mod,test[,-167])
+real=test[,167]
+
+###Print the confusion matrix
+res=table(predicted=prd,reality=real)
+print(res)
+
+###Accuracy = ?
+cat(str_c("Accuracy is ",sum(diag(res))/sum(res),"\n"))
 
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Aspartate_Racemase_Ligands_Bioactivity_Prediction
- This scripts tries to predict the bioactivity of 131 compounds related to Aspartate Racemase enzyme with the aid of decision trees
+ This scripts tries to predict the bioactivity of 131 small molecule compounds (taken from ChEMBL) related to Aspartate Racemase enzyme with the aid of MACCS166 Chemical Fingerprints (from *Rcpi* package) and the Decision Tree algorithm (from *rpart* package).
